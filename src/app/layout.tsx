8ef59a0b9d91a38f8822304088250def4228a445
wpeckelsen <wpeckelsen@gmail.com>
2024-09-26 11:39:10 +0200

    hello world page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -28,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+<h1>Wessel: Hello world</h1>
+        </nav>
         {children}
       </body>
     </html>
